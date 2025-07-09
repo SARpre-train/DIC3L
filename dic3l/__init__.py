22058253d483d860fd899eddb49d08453d281f5a
kiwi <73239711+Kiwi-xd@users.noreply.github.com>
2025-07-09 13:50:48 +0800

    Delete dic3l directory
    1

--- dic3l/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Shaanxi Yuanyi Intelligent Technology Co., Ltd.
-# This file is part of a project licensed under the MIT License.
-# It is developed based on the MoCo project by Meta Platforms, Inc.
-# Original MoCo repository: https://github.com/facebookresearch/moco
-#
-# This project includes significant modifications tailored for SAR land-cover classification,
-# including the design of domain-specific modules and the use of large-scale SAR datasets
-# to improve performance and generalization on downstream SAR tasks.
